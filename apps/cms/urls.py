f028374dc1e30a66d28b89d2f59dba7d048b8875
Null <546464268@qq.com>
2020-02-22 00:41:35 +0800

    CMS新闻分类模板定义

--- apps/cms/urls.py
+++ apps/cms/urls.py
@@ -5,5 +5,7 @@ app_name = 'cms'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('write_news/', views.WriteNews.as_view(), name='write_news'),
+    path('news_category/', views.news_category, name='news_category'),
     path('login/', views.login_view, name='login')
 ]
--- apps/cms/views.py
+++ apps/cms/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
+from django.views.generic import View
 
 
 def login_view(request):
@@ -9,3 +10,13 @@ def login_view(request):
 @staff_member_required(login_url='news:index')
 def index(request):
     return render(request, 'cms/index.html')
+
+
+class WriteNews(View):
+
+    def get(self, request):
+        return render(request, 'cms/write_news.html')
+
+
+def news_category(request):
+    return render(request, 'cms/news_category.html')
